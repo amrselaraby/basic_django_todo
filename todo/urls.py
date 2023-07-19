e9dd06424014209fd95180684edeb2af92ef8545
Amr Elaraby <amrselaraby@gmail.com>
2023-07-19 06:37:20 +0300

    todo app finished

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("addTask", views.addTask, name="addTask"),
+    path("addTask/", views.addTask, name="addTask"),
+    path("complete/<int:pk>/", views.complete_task, name="complete_task"),
+    path("incomplete/<int:pk>/", views.incomplete_task, name="incomplete_task"),
+    path("edit_task/<int:pk>/", views.edit_task, name="edit_task"),
+    path("delete_task/<int:pk>/", views.delete_task, name="delete_task"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse
 from .models import Task
 
@@ -6,4 +6,39 @@ from .models import Task
 def addTask(request):
     task = request.POST["task"]
     Task.objects.create(task=task)
+    return redirect("home")
+
+
+def complete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect("home")
+
+
+def incomplete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect("home")
+
+
+def edit_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        updated_task = request.POST["task"]
+        task.task = updated_task
+        task.save()
+        return redirect("home")
+
+    else:
+        context = {
+            "task": task,
+        }
+        return render(request, "edit_task.html", context)
+
+
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
     return redirect("home")
